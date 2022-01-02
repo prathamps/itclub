716497b457a0a984918887e8ac043a2f7276d5b3
Pratham P S <pspratham16@gmail.com>
2022-01-02 23:41:48 +0530

    block intial stages

--- public/game/block.js
+++ public/game/block.js
@@ -1,7 +1,19 @@
 function init() {
+	var option = 0;
 	var drake = dragula(
 		[document.getElementById("to-do"), document.getElementById("doing")],
 		{
+			moves: function (el, source, handle, sibling) {
+				return el.classList[1] === "question-code" ? false : true;
+			},
+			accepts: function (el, target, source, sibling) {
+				if (sibling)
+					return sibling.classList[1] === "question-code" &&
+						sibling.classList[2] !== "end-block"
+						? false
+						: true;
+				else return target.id == "doing" ? false : true;
+			},
 			revertOnSpill: true,
 			mirrorContainer: document.body,
 		}
@@ -9,7 +21,9 @@ function init() {
 
 	drake.on("drop", (e1, target, source, sibling) => {
 		if (target.id == "doing") {
-			console.log("");
+			if (e1.id === "optionOne") {
+				console.log("Yay... Correct answer");
+			}
 		}
 	});
 }
