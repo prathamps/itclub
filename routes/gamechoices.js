530e61c30e50b5efe18c447a161ca53e7dc7d331
Pratham P S <pspratham16@gmail.com>
2022-07-06 09:34:35 +0530

    Added the ping pong game

--- routes/gamechoices.js
+++ routes/gamechoices.js
@@ -1,48 +1,57 @@
 const express = require("express"),
 	router = express.Router(),
 	User = require("../models/user.js"),
-	middleware = require("../middleware");
+	middleware = require("../middleware")
 
 //Snake Game
 router.get("/tatakaesnake", (req, res) => {
-	if (!req.isAuthenticated()) return res.redirect("/login");
+	if (!req.isAuthenticated()) return res.redirect("/login")
 	res.render("./games/snakegame", {
 		title: "TatakaeSnake",
 		score: req.user.score,
 		id: req.user._id,
-	});
-});
+	})
+})
 
 router.put("/tatakaesnake/:id/updatescore", (req, res) => {
-	if (!req.isAuthenticated()) return res.redirect("/login");
-	let updUser = req.user;
-	updUser.score = req.user.score + parseInt(req.body.score);
-	console.log(updUser.score);
+	if (!req.isAuthenticated()) return res.redirect("/login")
+	let updUser = req.user
+	updUser.score = req.user.score + parseInt(req.body.score)
+	console.log(updUser.score)
 	User.findByIdAndUpdate(req.params.id, updUser, (err, updatedPlayerStats) => {
 		if (err) {
-			res.redirect("back");
+			res.redirect("back")
 		} else {
-			res.redirect("/leaderboards");
+			res.redirect("/leaderboards")
 		}
-	});
-});
+	})
+})
 
 //Block Snippets
 router.get("/snapthatcode", (req, res) => {
-	if (!req.isAuthenticated()) return res.redirect("/login");
+	if (!req.isAuthenticated()) return res.redirect("/login")
 	res.render("./games/blocksnippets", {
 		title: "SnapThatCode",
 		id: req.user._id,
-	});
-});
+	})
+})
 
 //Quiz
 router.get("/quiz", (req, res) => {
-	if (!req.isAuthenticated()) return res.redirect("/login");
+	if (!req.isAuthenticated()) return res.redirect("/login")
 	res.render("./games/quiz", {
 		title: "Take em Quiz",
 		id: req.user._id,
-	});
-});
+	})
+})
 
-module.exports = router;
+//Ping Pong
+router.get("/pingpong", (req, res) => {
+	if (!req.isAuthenticated()) return res.redirect("/login")
+	res.render("./games/pingpong", {
+		title: "Ping Wong",
+		id: req.user._id,
+	})
+})
+
+module.exports = router
